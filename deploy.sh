7ad14a235663f98b3d83bdccdae27aa9f752ee5e
Ubuntu <ubuntu@ip-172-31-7-2.ap-northeast-1.compute.internal>
2023-03-09 20:46:24 +0000

    correct the data

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ chmod +x build.sh
 ./build.sh
 docker login --username=$docker_username --password=$docker_password
 echo $docker_password | docker login -u $docker_username --password-stdin
-docker image push jeeviarajsam/reactapp_dev:v1
+docker image push jeeviarajsam/reactapp_production:v1
 else
 echo "error data"
 fi

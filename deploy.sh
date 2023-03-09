1a50245f6cb3942658645e7f74eb51b2db25de54
Ubuntu <ubuntu@ip-172-31-7-2.ap-northeast-1.compute.internal>
2023-03-09 19:32:07 +0000

    deploy script prepared

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#deploy the application
+ls -l
+mkdir react_app && mv -f * react_app
+tar -cvzf react_app.tar.gz react_app
+scp -o StrictHostKeyChecking=no -i $key react_app.tar.gz ubuntu@3.112.2.162:/home/ubuntu
+ssh -T -o StrictHostKeyChecking=no -i $key ubuntu@3.112.2.162<<EOF
+cd /home/ubuntu
+rm -rf react_app || true
+sudo docker system prune -y
+tar -xvzf react_app.tar.gz 
+#deploy based on git hub branch commit
+if [[ $GIT_BRANCH == origin/dev ]]
+then
+cd react_app
+chmod +x build.sh
+./build.sh
+docker login --username=$docker_username --password=$docker_password
+echo $docker_password | docker login -u $docker_username --password-stdin
+docker image push jeeviarajsam/reactapp_dev:v1
+elif [[ $GIT_BRANCH == origin/master ]]
+then
+cd react_app
+chmod +x build.sh
+./build.sh
+docker login --username=$docker_username --password=$docker_password
+echo $docker_password | docker login -u $docker_username --password-stdin
+docker image push jeeviarajsam/reactapp_dev:v1
+else
+echo "error data"
+fi

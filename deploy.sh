1a839e8068e3227aaae92de7331787a6c803d48d
Ubuntu <ubuntu@ip-172-31-7-2.ap-northeast-1.compute.internal>
2023-03-10 08:31:51 +0000

    this is updated one

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #deploy the application
-ls -l
-mkdir react_app && mv -f * react_app
+ls -la
+mkdir react_app && cp -rf public src Dockerfile build.sh deploy.sh docker-compose.yml package-lock.json package.json react_app 
 tar -cvzf react_app.tar.gz react_app
 scp -o StrictHostKeyChecking=no -i $key react_app.tar.gz ubuntu@43.207.231.100:/home/ubuntu
 ssh -T -o StrictHostKeyChecking=no -i $key ubuntu@43.207.231.100<<EOF
 cd /home/ubuntu
 rm -rf react_app || true
-sudo docker system prune -y
+sudo docker system prune -f
 tar -xvzf react_app.tar.gz 
 #deploy based on git hub branch commit
 if [[ $GIT_BRANCH == origin/dev ]]
@@ -20,6 +20,7 @@ echo $docker_password | docker login -u $docker_username --password-stdin
 docker image tag reactapp:v1 jeeviarajsam/reactapp_dev:v1
 docker image push jeeviarajsam/reactapp_dev:v1
 elif [[ $GIT_BRANCH == origin/master ]]
+#docker-compose up -d
 then
 cd react_app
 chmod +x build.sh
@@ -28,6 +29,8 @@ docker login --username=$docker_username --password=$docker_password
 echo $docker_password | docker login -u $docker_username --password-stdin
 docker image tag reactapp:v1 jeeviarajsam/reactapp_production:v1
 docker image push jeeviarajsam/reactapp_production:v1
+#docker-compose up -d
 else
 echo "error data"
 fi
+
